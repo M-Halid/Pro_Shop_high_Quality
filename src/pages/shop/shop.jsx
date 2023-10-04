b76ebf84861efbdfc99650789d600e7d63776e31
Muhammed Halid Kutsal <halid@MacBook-Pro-von-Halid.local>
2023-10-05 00:43:42 +0200

    initial commit

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -7,7 +7,7 @@ export const Shop = () => {
   return (
     <div className="shop">
       <div className="shopTitle">
-        <h1>PedroTech Shop</h1>
+        <h1>Pro Shop High Quality</h1>
       </div>
 
       <div className="products">
